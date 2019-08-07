2969ca16b7c58cde177a71a99ba1af38c64bb538
Knackish <42196394+knackish@users.noreply.github.com>
2019-08-07 17:27:28 -0500

    completed proj

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 void main() {
   return runApp(
@@ -15,9 +17,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  
+    int _leftDiceNum = 1;
+    int _rightDiceNum = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Center(
+      child: Row( 
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                 leftDiceNum = Random().nextInt(6)+1;
+                });
+              },
+              child: Image.asset('images/dice$_leftDiceNum.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                 rightDiceNum = Random().nextInt(6)+1; 
+                });
+              },
+              child: Image.asset('images/dice$_rightDiceNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
